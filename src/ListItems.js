603d68d2702460b8f0fa441878368883d91bb734
Thaneshwor <thaneshwor100@gmail.com>
2020-03-30 19:57:15 +0545

    Use trash image in list items

--- src/ListItems.js
+++ src/ListItems.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Trash from './trash.png'
 import './ListItems.css'
 
 function ListItems(props) {
     const items = props.items;
     const listItems = items.map(item => {
         return <div className='list' key={item.key}>
-            <p>{item.text}</p>
+            <p>{item.text}<span><img src={Trash} /></span></p>
         </div>
 
     })
